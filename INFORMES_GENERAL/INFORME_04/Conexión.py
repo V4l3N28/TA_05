ac18dd70238bdec26976f9dbcdfb75e215707035
Valentina Ortega Paz <80727758+V4l3N28@users.noreply.github.com>
2021-04-21 22:12:03 -0500

    Update Conexión.py

--- INFORMES_GENERAL/INFORME_04/Conexión.py
+++ INFORMES_GENERAL/INFORME_04/Conexión.py
@@ -8,3 +8,13 @@ app = Flask(__name__)
 def based():
     #Conexión a base de datos
     conexion= sqlite3.connect('basedatos.db')
+  #Crear cursor
+    cursor = conexion.cursor()
+    #Crear tabla
+    cursor.execute("CREATE TABLE IF NOT EXISTS datos_estacion_02("+
+    "id INTEGER PRIMARY KEY AUTOINCREMENT, "+
+    "fecha DATE, "+
+    "temperatura_min INTEGER, "+
+    "temperatura_max INTEGER, "+
+    "lluvias INTEGER"+
+    ")")
