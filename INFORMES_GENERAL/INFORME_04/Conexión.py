669bc3dfb5d4626ef92e834f41728e40e203fb40
Valentina Ortega Paz <80727758+V4l3N28@users.noreply.github.com>
2021-04-22 12:43:55 -0500

    Update Conexión.py
    Creación de tablas (fincas, observadores, registros, veredas) y conexión a sqlite3
    
    Co-Authored-By: kobando <80731419+kobando@users.noreply.github.com>
    Co-Authored-By: Isaac Mesa Maya <80772803+Isaacmesa92@users.noreply.github.com>

--- INFORMES_GENERAL/INFORME_04/Conexión.py
+++ INFORMES_GENERAL/INFORME_04/Conexión.py
@@ -1,49 +1,41 @@
 from flask import Flask,render_template
 import sqlite3
 app = Flask(__name__)
-#@app.route('/')
-#def index():
-#return render_template("main.html")
 
 def based():
     #Conexión a base de datos
     conexion= sqlite3.connect('basedatos.db')
+
     #Crear cursor
     cursor = conexion.cursor()
     #Crear tabla
-    cursor.execute("CREATE TABLE IF NOT EXISTS datos_estacion_02("+
-    "id INTEGER PRIMARY KEY AUTOINCREMENT, "+
-    "fecha DATE, "+
-    "temperatura_min INTEGER, "+
-    "temperatura_max INTEGER, "+
-    "lluvias INTEGER"+
-    ")")
-  #GUARDAR CAMBIOS
-    conexion.commit()
-#INSERTAR DATOS
-    #cursor.execute("INSERT INTO datos_estacion_02 VALUES (null, '06/04/2021', 4, 5, 9)")
-    conexion.commit()
-#LISTAR DATOS
-    cursor.execute("SELECT temperatura_max FROM datos_estacion_02;")
-    temperaturas_max = cursor.fetchall()
-    var1= [f[0] for f in temperaturas_max]
-    
-    cursor.execute("SELECT fecha FROM datos_estacion_02;")
-    fechas = cursor.fetchall()
-    var2= [f[0] for f in fechas]
-    
-    
-    #print(temperaturas_max,"   ", fechas)
-    #Cerrar conexion a base de datos
-    conexion.close()
+    cursor.execute("CREATE TABLE IF NOT EXISTS fincas("+
+	"id	INTEGER PRIMARY KEY AUTOINCREMENT, "+
+	"idArea	INTEGER NOT NULL FOREIGN KEY REFERENCES veredas(id),"+
+	"finca	INTEGER NOT NULL,"+
+	")")
 
-    return(var1, var2)
-@app.route('/')
-def temp():
-    etiqueta = based()
-    temperaturas_max = etiqueta[0]
-    fechas= etiqueta[1]
-    weekle_temps = [("lun",12),("mart",2),("mierc",20),("jue",23),("vier",8),("sab",12),("dom",30)]
-    labels = [row[0] for row in weekle_temps]
-    values = [row[1] for row in weekle_temps]
-    return render_template("index.html",labels =fechas, values=temperaturas_max)
+    cursor.execute("CREATE TABLE observadores ("+
+	"id	INTEGER PRIMARY KEY AUTOINCREMENT,"+
+	"primerNombre	TEXT NOT NULL,"+
+	"segundoNombre	TEXT,"+
+	"apellidos	TEXT NOT NULL,"+
+	"celular	INTEGER,"+
+	"latitud	NUMERIC,"+
+	"longitud	NUMERIC,"+
+	")")
+
+    cursor.execute("CREATE TABLE registros ("+
+	"id	INTEGER PRIMARY KEY AUTOINCREMENT,"+
+	"idFinca	INTEGER NOT NULL,"+
+	"fecha	TEXT,"+
+	"precipitacion	REAL,"+
+	"temperaturaMaxima	REAL,"+
+	"temperaturaMinima	REAL,"+
+	")")
+    cursor.execute("CREATE TABLE veredas ("+
+	"id	INTEGER PRIMARY KEY AUTOINCREMENT,"+
+	"departamento	TEXT NOT NULL,"+
+	"ciudad	TEXT NOT NULL,"+
+	"vereda	TEXT NOT NULL,"+
+	")")
